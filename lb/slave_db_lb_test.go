f4b3771806ed24b10dea23873c5b79b9c81007dd
wangqingpei <wangqingpei557@163.com>
2021-01-21 16:44:38 +0800

    修改go.mod

--- lb/slave_db_lb_test.go
+++ lb/slave_db_lb_test.go
@@ -2,40 +2,38 @@ package lb
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/gocraft/dbr"
+	"log"
 	"reflect"
 	"testing"
 )
 
-//concurrency test
-func BenchmarkSlaveLoadBalancer_GetPollingNode(t *testing.B) {
-	dbr0 := &sql.DB{}
-	dbr1 := &sql.DB{}
-	dbr2 := &sql.DB{}
-
-	slb := BuildSlaveLoadBalancer(15, 16, 1, dbr0, dbr1, dbr2)
-
-	i0 := 0
-	i1 := 0
-	i2 := 0
-	i3 := 0
-	hit := 0
-	for i := 0; i < t.N; i++ {
-		lbIndex, h, _ := slb.GetPollingNode()
-		if lbIndex == 0 {
-			i0++
-		} else if lbIndex == 1 {
-			i1++
-		} else if lbIndex == 2 {
-			i2++
-		} else if lbIndex == 3 {
-			i3++
+//main test.
+func TestIntegration(t *testing.T) {
+	var slaveLB *SlaveLoadBalancer
+	//构造数据源
+	creator := func() *sql.DB {
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&autocommit=1", "root", 123456, "localhost", 3306, "test", "utf8mb4")
+		conn, er := sql.Open("mysql", dsn)
+		if er != nil {
+			log.Fatalf("create db conn error:%v", er.Error())
 		}
-		if h {
-			hit++
-		}
+		return conn
 	}
 
-	t.Logf("0:%v,1:%v,2:%v,3:%v,hit:%v", i0, i1, i2, i3, hit)
+	slave1 := creator()
+	slave2 := creator()
+
+	//构造slave-lb
+	slaveLB = BuildSlaveLoadBalancer(1, 2, 1, slave1, slave2)
+
+	//获取数据源
+	_, _, conn := slaveLB.GetPollingNode()
+
+	//放入任意orm中，这里举例dbr
+	orm := &dbr.Connection{DB: conn}
+	orm.NewSession(nil).SelectBySql("select id from tb_orders limit 1")
 }
 
 //test connection append 顺序
@@ -70,4 +68,36 @@ func TestSlaveLoadBalance_JumpTimeRange(t *testing.T) {
 	hit := slb.HitJumpTimeRange(1)
 
 	t.Logf("hit:%v", hit)
+}
+
+//concurrency test
+func BenchmarkSlaveLoadBalancer_GetPollingNode(t *testing.B) {
+	dbr0 := &sql.DB{}
+	dbr1 := &sql.DB{}
+	dbr2 := &sql.DB{}
+
+	slb := BuildSlaveLoadBalancer(15, 16, 1, dbr0, dbr1, dbr2)
+
+	i0 := 0
+	i1 := 0
+	i2 := 0
+	i3 := 0
+	hit := 0
+	for i := 0; i < t.N; i++ {
+		lbIndex, h, _ := slb.GetPollingNode()
+		if lbIndex == 0 {
+			i0++
+		} else if lbIndex == 1 {
+			i1++
+		} else if lbIndex == 2 {
+			i2++
+		} else if lbIndex == 3 {
+			i3++
+		}
+		if h {
+			hit++
+		}
+	}
+
+	t.Logf("0:%v,1:%v,2:%v,3:%v,hit:%v", i0, i1, i2, i3, hit)
 }
